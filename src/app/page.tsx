fe4a9c703867e1a6f0a45b81da14f711df1472b8
wiseqingyang <wiseqingyang@163.com>
2026-05-25 18:27:39 +0800

    chore: remove unused routes and vercel code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,11 @@
-import 'bulma';
-
-import Link from 'next/link';
-
 import styles from './page.module.css';
 
-export default function Home() {
-  console.log('styles', styles);
+const Home = () => {
   return (
     <main className={styles.main}>
-      <div className={styles.grid}>
-        <Link
-          href="/lottie"
-          className={styles.card}
-          rel="noopener noreferrer"
-        >
-          <h2>Lottie 工具</h2>
-          <p>自动将 Lottie 中的图片文件转为 base64 </p>
-        </Link>
-        <Link
-          href="/shortID"
-          className={styles.card}
-          rel="noopener noreferrer"
-        >
-          <h2>短Id 生成</h2>
-          <p>缩短ID</p>
-        </Link>
-      </div>
+      <h1 className={styles.title}>B Tools</h1>
     </main>
   );
-}
+};
+
+export default Home;
